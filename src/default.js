ff3a77449eb92b11383d77972dfcbc55fc86a94b
Torres2235 <Torres2235@gmail.com>
2024-03-22 20:35:16 -0700

    all functionality except finished tab working

--- src/default.js
+++ src/default.js
@@ -8,10 +8,6 @@ export default function defaultStart() {
     createTodo(defaultProject, "Create new Todo Item", 'use the button below','2024-03-31','high');
     createTodo(defaultProject, "Create new Project Item", 'use the button on the left','2024-03-31','low');
 
-    let defaultProject2 = createProject("Work");
-    createTodo(defaultProject2, "tester");
-    createTodo(defaultProject2, "tester2");
-
     displayProject(defaultProject);
 
     return defaultProject;
